0bc84e885f5508ebaf9343649d784bb3a99a3a69
Chatchai Vichai <ee56054@gmail.com>
2019-12-10 22:36:38 +0700

    add thai_check_digit

---
+++ thai_check_digit.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+idcard = '3610300032039'
+# idcard = '1101234567890'
+idcard = np.array(list(idcard[:]), dtype=int)
+
+order = np.arange(13,1, -1)
+
+# order = np.flip(order)
+print(f'order => {order}')
+
+print(f'order.__len__:{order.__len__()}')
+
+idcard = idcard[:idcard.__len__()-1] 
+print(f'idcard {idcard}')
+result = idcard * order
+
+print(result)
+
+result  = np.sum(result) 
+print(result)
+result  = np.mod(result, 11)
+
+
+if result <= 1 : 
+    result = 1 - result 
+else :
+    result = 11 - result
+
+print(result)
