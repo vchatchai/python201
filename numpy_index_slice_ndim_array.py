9e17625ed3fde588391157a36edada02425a9637
Chatchai Vichai <ee56054@gmail.com>
2019-12-20 08:59:17 +0700

    add numpy_index_slice_ndim_array.py

---
+++ numpy_index_slice_ndim_array.py
@@ -0,0 +1,32 @@
+import numpy as np 
+
+print(np.__version__)
+
+a = np.reshape(np.arange(1,21), (4,5))
+print(a)
+
+print(f'a[1] => {a[1]}')
+
+print(f'a[3] => {a[1]}')
+
+print(f'a[-1] => {a[-1]}')
+
+
+print(f'a[1,2] => {a[1,2]}')
+print(f'a[1][2] => {a[1][2]}')
+
+print(list(np.ndenumerate(a)))
+
+print(f'a[0:2] => {a[0:2]}')
+
+print(f'a[2:] => {a[2:]}')
+
+print(f'a[:,2] => {a[:,2]}')
+
+print(f'a[2:4,0:2] => {a[2:4,0:2]}')
+print(f'a[2:,:2] => {a[2:,:2]}')
+
+print(f'a[:,3:] => {a[:,3:]}')
+
+print(f'a[::2] =>{a[::2]}')
+print(f'a[1::2] => {a[1::2]}')
